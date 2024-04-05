846d1ca512f52da05fdeb9be8c3eb4dd9375060b
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-04-05 11:33:47 -0700

    Todo Trace List is a simple yet powerful task management tool designed to help individuals or teams organize, track, and prioritize their tasks efficiently. With intuitive features and a user-friendly interface, managing your to-do lists has never been easier.
    Todo Trace List is a simple yet powerful task management tool designed to help individuals or teams organize, track, and prioritize their tasks efficiently. With intuitive features and a user-friendly interface, managing your to-do lists has never been easier.

---
+++ todo.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+let loop = true;
+
+while(loop){
+    const answers : {
+        TODO: string,
+        addMore: boolean,
+    } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "what do you want to add in your todo? "
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want to add more todo? ",
+            default: false
+        }
+    ])
+    const {TODO , addMore} = answers;
+    console.log(answers)
+    loop = addMore
+    if(TODO) {
+       todos.push(TODO) 
+    } else {
+        console.log("kindly add valid input")
+    }
+}
+
+if(todos.length > 0) {
+    console.log("Your TODO List:")
+todos.forEach(TODO => {
+    console.log(TODO)
+});
+} else {
+    console.log("NO todos found")
+}
